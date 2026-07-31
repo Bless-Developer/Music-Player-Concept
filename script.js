a510d525a9438791fd99ff02212adb5753302429
Bless <wastoggezz@gmail.com>
2026-07-31 12:02:05 -0300

    V0.3 - Adição de mais elementos JavaScript

--- script.js
+++ script.js
@@ -2,16 +2,32 @@ console.log("Player started");
 
 const music = document.getElementById("music");
 const btnPause = document.getElementById("pause");
+
 let tocando = true;
 
 btnPause.addEventListener("click", () => {
     if (tocando === false){
-        btnPause.innerHTML = "Play";
+        btnPause.innerHTML = "▶";
         music.pause();
         tocando = true;
     } else{
-        btnPause.innerHTML = "Pause";
+        btnPause.innerHTML = "⏸";
         music.play();
         tocando = false;
     }
+});
+
+const barraProgress = document.getElementById("barra-prog");
+
+music.addEventListener("timeupdate", () =>{
+    const percent = 
+    (music.currentTime / music.duration) * 100;
+
+    barraProgress.style.width = `${percent}%`; 
+});
+
+const volume = document.getElementById("volume");
+
+volume.addEventListener("input", () =>{
+    music.volume = volume.value;
 });
